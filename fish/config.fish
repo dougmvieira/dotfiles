badd3ff68ee019bdeaaacbbc3af78cfbb186526a
Doug Vieira <dougmvieira@gmail.com>
2025-05-28 01:40:23 +0100

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,4 @@
 set fish_greeting
 alias copy='xclip -sel clip'
+set -U fish_user_paths $HOME/.local/bin $HOME/.cabal/bin $HOME/.cargo/bin $fish_user_paths
+alias kittyupdate='curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin'
